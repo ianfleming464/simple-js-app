3b1232fa1e8ca29435f70f5782f8d5a884674090
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-02-07 23:10:12 +0100

    Created scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+alert('Hello World!');
+
+var myName = 'Ian';
+document.write(myName);
+// myName = 'Donald Trump Jr.';
+// document.write(myName);
+
+var simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+var favouriteFood = 'Pizza';
+document.write(favouriteFood);
