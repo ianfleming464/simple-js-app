e157dee15140dc14edd2c494150e7b68df4eb5b5
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-02-08 23:51:41 +0100

    Created repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,8 @@
-alert('Hello World!');
-
-var myName = 'Ian';
-document.write(myName);
-// myName = 'Donald Trump Jr.';
-// document.write(myName);
-
-var simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-var size = 100;
-var doubleSize = size * 2;
-var minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-
-var favouriteFood = 'Pizza';
-document.write(favouriteFood);
+var repository = [
+  { name: 'Snorlax', height: 6.11, types: ['normal'] },
+  { name: 'Jigglypuff', height: 1.08, types: ['normal', 'fairy'] },
+  { name: 'Charizard', height: 5.07, types: ['fire', 'flying'] },
+  { name: 'Empoleon', height: 5.07, types: ['water', 'steel'] },
+  { name: 'Psyduck', height: 2.07, types: ['water'] },
+  { name: 'Jynx', height: 4.07, types: ['ice', 'psychic'] }
+];
