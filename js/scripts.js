87c638c46200b22d68ee26c0ba8ae89f008808b1
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-02-19 22:02:48 +0100

    Updated scripts.js
    Wrapped repository in IIFE, deleted for loop, added return functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
-var repository = [
-  { name: 'Snorlax', height: 6.11, types: ['normal'] },
-  { name: 'Jigglypuff', height: 1.08, types: ['normal', 'fairy'] },
-  { name: 'Charizard', height: 5.07, types: ['fire', 'flying'] },
-  { name: 'Empoleon', height: 5.07, types: ['water', 'steel'] },
-  { name: 'Psyduck', height: 2.07, types: ['water'] },
-  { name: 'Jynx', height: 4.07, types: ['ice', 'psychic'] } //6 item array //
-];
+var pokemonRepository = (function () { //IIFE
+  var repository = [
+    { name: 'Snorlax', height: 6.11, type: ['normal'] },
+    { name: 'Jigglypuff', height: 1.08, type: ['normal', 'fairy'] },
+    { name: 'Charizard', height: 5.07, type: ['fire', 'flying'] },
+    { name: 'Empoleon', height: 5.07, type: ['water', 'steel'] },
+    { name: 'Psyduck', height: 2.07, type: ['water'] },
+    { name: 'Jynx', height: 4.07, type: ['ice', 'psychic'] }
+  ];
 
-// Using a for loop:
-// for (var i = 0; i < repository.length; i++) {
-//   var pokemon = repository[i];
-//   if (pokemon.height >= 6) {
-//     document.write(pokemon.name + ' ' + ' (height: ' + pokemon.height + ') ' + ' - Wow! That\s big' + '<br>');
-//   } else {
-//     document.write(pokemon.name + ' ' + ' (height: ' + pokemon.height + ') ' + '<br>');
-//   }
-// }
+    return {
+      add: function(name, type, height) {
+        repository.push(name, type, height);
+      },
+      getAll: function() {
+        return repository;
+      }
+    };
+})();
+
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Caterpie', height: 1.00, type: ['bug'] });
+console.log(pokemonRepository.getAll());
 
 // Using a forEach loop:
 repository.forEach(function(currentPokemon){
