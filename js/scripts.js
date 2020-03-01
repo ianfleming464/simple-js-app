b04b1a02ec3a5654244c3ea129997aa266fe075c
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-03-01 22:50:39 +0100

    Update scripts.js
    Stopped console.logging the details.
    Added a loop to (in turn) add the Pokemon types.
    Added the code (from my CodePen messaround) to successfully add a modal which behaves as required.
    Updated return functions.
    
    Huzzah!

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var pokemonRepository = (function () { //IIFE
   var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  var $modalContainer = document.querySelector('#modal-container');
 
   function add(newPokemon) {
     repository.push(newPokemon);
@@ -11,14 +12,14 @@ var pokemonRepository = (function () { //IIFE
   }
 
   function addListItem(pokemon) {
-    var $pokeList = document.querySelector('.pokemon-list');
-    var button = document.createElement('button');
+    var $pokeList = document.querySelector('.pokemon-list'); // the ul from HTML
+    var $button = document.createElement('button');
     var $listItem = document.createElement('li');
-    button.innerText = pokemon.name;
-    button.classList.add('button-class');
-    $listItem.appendChild(button);
+    $button.innerText = pokemon.name;
+    $button.classList.add('button-class');
+    $listItem.appendChild($button);
     $pokeList.appendChild($listItem);
-    button.addEventListener('click', function(event) {
+    $button.addEventListener('click', function(event) {
       showDetails(pokemon);
     }
   );
@@ -26,7 +27,8 @@ var pokemonRepository = (function () { //IIFE
 
 function showDetails(item) {
   pokemonRepository.loadDetails(item).then(function() {
-    console.log(item);
+    // console.log(item);
+    showModal(item);
   });
 }
 
@@ -46,18 +48,95 @@ function loadList() {
   });
 }
 
-function loadDetails(item) {
+function loadDetails(item) { // Loading the details from the API
   var url = item.detailsUrl;
   return fetch(url).then(function(response) {
     return response.json();
   }).then(function(details) { //Now we add the details to the items
     item.imageUrl = details.sprites.front_default;
     item.height = details.height;
-    item.types = Object.keys(details.types);
+    // item.types = Object.keys(details.types); //this returns an array of details
+    item.types = []; // Loop to go through the types and add them, if there's more than 1
+        for (var i = 0; i < details.types.length; i++) {
+          item.types.push(details.types[i].type.name);
+        }
   }).catch(function(e) {
     console.error(e);
   });
 }
+
+function showModal(item) {
+  // Clear all existing modal content
+
+  $modalContainer.innerHTML = '';
+
+  var modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  // Makes modal appear
+  $modalContainer.classList.add('is-visible');
+
+  // Closes modal upon clicking close button
+  var closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.style.fontFamily = 'Play';
+  closeButtonElement.addEventListener('click', hideModal);
+
+  // This will be the Pokemon's name element
+  var nameElement = document.createElement('h1');
+  nameElement.innerText = item.name;
+
+  // This will be the Pokemon's height element
+  var heightElement = document.createElement('p');
+  heightElement.innerText = 'Height: ' + item.height;
+
+  // Image - able for a less pixelated verision? ASK
+  var imageElement = document.createElement('img');
+  imageElement.classList.add('modal-img');
+  imageElement.setAttribute('src', item.imageUrl);
+  // Types?
+  var typesElement = document.createElement('p');
+  typesElement.innerText = 'Types: ' + ' ' + item.types;
+
+  // ADding the different bits to the modal itself in the DOM
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(nameElement);
+  modal.appendChild(typesElement);
+  modal.appendChild(heightElement);
+  modal.appendChild(imageElement);
+  $modalContainer.appendChild(modal);
+
+  $modalContainer.classList.add('is-visible');
+}
+
+// var dialogPromiseReject; //This can be set later by showDialog
+
+function hideModal() {
+  $modalContainer.classList.remove('is-visible');
+
+  if (dialogPromiseReject) {
+    dialogPromiseReject();
+    dialogPromiseReject = null;
+  }
+}
+
+window.addEventListener('keydown', e => {
+  if (
+    e.key === 'Escape' &&
+    $modalContainer.classList.contains('is-visible')
+  ) {
+    hideModal();
+  }
+});
+
+$modalContainer.addEventListener('click', e => {
+  var target = e.target;
+  if (target === $modalContainer) {
+    hideModal();
+  }
+});
+
 
 return { //the keys: IIFE functions; the values: what the outside world knows them as
   add: add,
@@ -65,8 +144,12 @@ return { //the keys: IIFE functions; the values: what the outside world knows th
   addListItem: addListItem,
   loadList: loadList,
   loadDetails: loadDetails,
-  showDetails: showDetails
+  showDetails: showDetails,
+  showModal: showModal,
+  hideModal: hideModal
+
 };
+
 })();
 
 pokemonRepository.loadList().then(function() {   // Now the data is loaded
